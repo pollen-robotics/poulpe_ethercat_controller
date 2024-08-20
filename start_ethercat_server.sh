2ef46dc7d968d0423b6bf8165f2b8bbf75d27cc6
RemiFabre <remifabre1800@gmail.com>
2024-08-20 06:35:29 +0000

    FIX kill the grpc server before starting it

--- start_ethercat_server.sh
+++ start_ethercat_server.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
 
+## kill the old server if already running
+## this line should not do anything in the normal circumstances
+## TODO: make sure that the server never stays on
+pkill "server"
+
+## start the grpc server
 cd $HOME/dev/poulpe_ethercat_controller
 RUST_LOG=info ./target/release/server config/robot.yaml
